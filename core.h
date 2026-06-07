830729e79297f97369a9a84760287873f38bea1c
Pavel-Sedlovec <pasha.sedlovetz@gmail.com>
2026-06-07 12:18:09 +0300

    Create core.h

---
+++ core.h
@@ -0,0 +1,47 @@
+#include<string>
+#include<vector>
+#ifndef CORE_H
+#define CORE_H
+
+
+enum TokenType{
+    MULL,
+    DIV,
+    PLUS,
+    MINUS,
+    NUMBER,
+    LPAREN,
+    RPAREN
+};
+
+struct Token{
+    TokenType type;
+    double value;
+};
+
+class Core
+{
+public:
+    Core();
+};
+
+class Parser{
+private:
+    int pos;
+    std::vector<Token> tokens;
+public:
+    Parser(std::vector<Token> tokens);
+
+    double parse();
+
+    double expression();
+
+    double term();
+
+    double factor();
+};
+
+
+std::vector<Token> tokenize(std::string str);
+
+#endif // CORE_H
