da4f898ba4a16bece426bc7e61ce769994edfc88
Pavel-Sedlovec <pasha.sedlovetz@gmail.com>
2026-06-07 12:18:44 +0300

    Create mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,44 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QtWidgets>
+#include <QObject>
+
+#include "mybutton.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    int counter;
+    QWidget *m_widget;
+    QGridLayout *m_grid;
+    QPushButton *m_num_1;
+    QPushButton *m_num_2;
+    QPushButton *m_num_3;
+    QPushButton *m_num_4;
+    QPushButton *m_num_5;
+    QPushButton *m_num_6;
+    QPushButton *m_num_7;
+    QPushButton *m_num_8;
+    QPushButton *m_num_9;
+    QPushButton *m_num_0;
+
+    QPushButton *m_div;
+    QPushButton *m_sum;
+    QPushButton *m_minus;
+    QPushButton *m_multiply;
+    QPushButton *m_equally;
+
+    QLineEdit *m_lineEdit;
+    MainWindow(QWidget *parent = nullptr);
+
+    void OnClick();
+    void onClick_equally();
+
+    ~MainWindow();
+};
+#endif // MAINWINDOW_H
